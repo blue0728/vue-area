fe0fa27e30ef8f8fda549900b367a3bb779e6382
spikef <spikef@foxmail.com>
2016-08-15 17:18:57 +0800

    修改动画为transform, 修正demo中的链接地址

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 //启动服务
+var port = 8642;
 var app = new WebpackDevServer(webpack(config), {});
-app.listen(8642, function() {
-	console.log('start server：http://localhost:9100')
+app.listen(port, function() {
+	console.log('start server：http://localhost:%s', port)
 });
